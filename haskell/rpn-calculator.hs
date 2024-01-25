01cb4e12fbece3f686c040cb9deffcef03686613
naturefx <alsmrqs@outlook.com>
2024-01-25 07:49:17 -0300

    commeting parser

--- haskell/rpn-calculator.hs
+++ haskell/rpn-calculator.hs
@@ -7,19 +7,20 @@ main :: IO ()
 main = do
     putStr "RPN Calculator: "
     frame <- getLine >>= return . words
-    if parsing frame 0 then 
-        print $ start frame (Stack [])
-    else putStrLn "Invalid input!"
+
+    if not $ parsing frame 0 then putStrLn "Invalid input!"
+    else print $ start frame (Stack [])
+
     main
 
 type Frame = [String]
 
-parsing :: Frame -> Int -> Bool
-parsing []     1 = True
-parsing []     _ = False
+parsing :: Frame -> Int -> Bool 
+parsing []     1 = True       -- stack must end with (1) element
+parsing []     _ = False      -- any other quantity 
 parsing (x:xs) n
-    | not $ operator x = bool False (parsing xs $ n+1) $ number x
-    | otherwise        = bool False (parsing xs $ n-1) $ n >= 2
+    | not $ operator x = bool False (parsing xs $ n+1) $ number x -- if [x1..] is number   stack.length (+1)
+    | otherwise        = bool False (parsing xs $ n-1) $ n >= 2   -- if [x1..] is operator stack.length (-1)
 
 operator :: [Char] -> Bool
 operator = (`elem` ["+","-","*","/"])
